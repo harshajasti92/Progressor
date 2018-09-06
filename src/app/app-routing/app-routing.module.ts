43340078fe27385c5f929068f343284bbe52a6c9
harsha <harshajasti92@gmail.com>
2018-09-06 15:38:15 -0400

    fixed bugs in about us page

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { StartPageComponent } from '../start-page/start-page.component'
 import { LoginPageComponent } from '../login-page/login-page.component'
 import { HomePageComponent } from '../home-page/home-page.component'
+import { AboutUsPageComponent } from '../about-us-page/about-us-page.component'
 
 const routes: Routes = [
   { path: '', component: StartPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'home', component: HomePageComponent },
+  { path: 'aboutUs', component: AboutUsPageComponent},
 
 ];
 
